071164afff865113512d6b4377e4793aeb167ca9
Stephen Way <stephencway@me.com>
2013-02-22 08:39:21 -0800

    Modified zsh completions and twitter follower count

--- completions/agency.zsh
+++ completions/agency.zsh
@@ -17,3 +17,27 @@ _agency() {
 
   reply=("${(ps:\n:)completions}")
 }
+
+# Sharepoint
+alias sharepoint="$BROWSER 'http://sharepoint3'"
+
+# Timeforce
+alias timeforce="$BROWSER 'http://bend.quantumclub.com/qqest/Login/login.asp'"
+alias timeforcepunch="$BROWSER http://bend.quantumclub.com/qqest/time/timeCard.asp && $BROWSER 'http://bend.quantumclub.com/qqest/Time/timePunch.asp?punchtype=clockinout&ActiveEmployeeID=57'"
+
+# Zendesk
+alias zendesk="$BROWSER 'https://agencyrevolution.zendesk.com/agent'"
+alias knowledgebase="$BROWSER 'https://agencyrevolution.zendesk.com/forums'"
+
+# Common directories
+alias portals="cd ~/Shares/AR-Clients/Portals && ls"
+alias assets="cd ~/Shares/AR-Clients/Portals/_assets && ls"
+alias skins="cd ~/Shares/AR-Clients/Portals/_default/Skins && ls"
+alias modules="cd ~/Shares/AR-Clients/DesktopModules && ls"
+alias remotegems="cd ~/Shares/Ruby/lib/ruby/gems/1.9.1/gems && ls"
+alias qdrive="cd ~/Shares/Groups && ls"
+
+# Skins
+for skin in susy echo; do
+  alias $skin="skins && $EDITOR $skin"
+done
